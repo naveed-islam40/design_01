9fcfe0fba2015516b5d54adb786ffcb878b51c87
naveed-islam40 <naveedislam20ni@gmail.com>
2025-03-10 23:04:14 +0500

    changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,10 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+module.exports = {
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+  }
+}
+
 export default eslintConfig;
